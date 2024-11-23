3dd03eecd9a9ffb21e7c01b61ca216bff7a13361
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-11-24 00:02:20 +0200

    create ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
 
 function App() {
-    const [likes, setLikes] = useState(0);
-    const [value, setValue] = useState("Текст в інпуті");
+    // const [value, setValue] = useState("Текст в інпуті");
 
-    function increment() {
-        setLikes(likes + 1);
-    }
-    function decrement() {
-        setLikes(likes - 1);
-    }
     return (
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <input 
-              type="text" 
-              value={value} 
-              onChange={event => setValue(event.target.value)}
-            />
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            {/* <Counter /> */}
+            <ClassCounter />
         </div>
     );
 }
