4a2d8669cc1583af8afbd86b019c5dcf3ad2af83
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-11-24 15:36:11 +0200

    create input/button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-// import { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
-import Counter from "./components/Counter";
+import React, { useRef, useState } from "react";
+import PostList from "./components/PostList";
+import "./components/styles/App.css";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-    // const [value, setValue] = useState("Текст в інпуті");
+    const [posts, setPosts] = useState([
+        { id: 1, title: "JavaScript", body: "Description" },
+        { id: 2, title: "React", body: "A library for building UI" },
+        { id: 3, title: "Node.js", body: "JavaScript runtime" },
+    ]);
+
+    const [post, setPost] = useState({ title: "", body: "" });
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        setPosts([...posts, {...post, id: Date.now()}]);
+        setPost({ title: "", body: "" });
+    };
 
     return (
         <div className="App">
-            {/* <Counter /> */}
-            <ClassCounter />
+            <form>
+                <MyInput
+                    value={post.title}
+                    onChange={(e) =>
+                        setPost({ ...post, title: e.target.value })
+                    }
+                    type="text"
+                    placeholder="name post"
+                />
+                <MyInput
+                    value={post.body}
+                    onChange={(e) => setPost({ ...post, body: e.target.value })}
+                    type="text"
+                    placeholder="description post"
+                />
+                <MyButton onClick={addNewPost}>create post</MyButton>
+            </form>
+            <PostList posts={posts} title="List post 1" />
         </div>
     );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+const PostList = ({ posts, title }) => {
+    if (!posts || posts.length === 0) {
+        return <h2 style={{ textAlign: "center" }}>No posts available</h2>;
+    }
+
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>{title}</h1>
+            {posts.map((post, index) => (
+                <PostItem number={index + 1} post={post} key={post.id} />
+            ))}
+        </div>
+    );
+};
+
+export default PostList;
